45214ab166bf731d3ca92c896a0eada36cecba9f
houssston <bobru3@gmail.com>
2021-05-12 01:32:36 +0300

    Сделал поиск и API для него

--- src/api/api.js
+++ src/api/api.js
@@ -1,22 +1,18 @@
 import * as axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'TEMPALTE',
-        header:{
-
-        },
-        params: {
+    baseURL: 'https://api.themoviedb.org/3/',
+    params: {
+        "api_key": "7bf0ddbd0b708dd904d550607793fa52",
+        "language": "ru"
     }
 });
 
-export const TEMPALTE = {
-    TEMPALTEquery(query){
-        return instance.get(``)
+export const tmdbAPI = {
+    getPersonList(query){
+        return instance.get(`search/person?language=en-US&query=${query}&page=1&include_adult=true`)
             .then(response => {
                 return response.data;
             });
     }
 };
-
-
-
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {tmdbAPI} from "../../api/api";
 
 const Home = () => {
-    return (
-        <div>
+    const [personNameList, setPersonNameList] = useState([]);
+    const [personName, setPersonName] = useState("");
 
-        </div>
+    //const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+    async function getPersonList(event) {
+        setPersonName(event.target.value);
+        const response = await tmdbAPI.getPersonList(personName);
+        setPersonNameList(response.results);
+    }
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <>
+            <input type="text" value={personName} onChange={event => getPersonList(event)}/>
+            <ul>
+                {personName.length > 0
+                && personNameList.map(item => (
+                    <li key={item.id}>
+                        {item.name}
+                    </li>
+                ))
+                }
+            </ul>
+        </>
     );
 };
 
