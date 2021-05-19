b5cd576a8c37e9e3f0b466197b9a32621e5bf8fa
houssston <bobru3@gmail.com>
2021-05-19 16:42:51 +0300

    Добавил Style для Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import logo from '../../assets/img/logo.png';
+import s from './Header.module.css';
+
 
 const Header = () => {
+
     return (
-        <div>
-            Find Film
-        </div>
+        <header className={s.header}>
+            <div className={s.wrapper}>
+                <div className={s.content}>
+                    <a className={s.logo} href={'/'}>
+                        <img className={s.logo__img} src={logo} alt={'Logo'}/>
+                    </a>
+                </div>
+            </div>
+        </header>
     );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import {tmdbAPI} from "../../api/api";
 
+import background1 from "../../assets/img/back1.jpg"
+
 const Home = (props) => {
     const [actorName, setActorName] = useState("");
     const [debounce, setDebounce] = useState(actorName);
@@ -18,7 +20,7 @@ const Home = (props) => {
 
     useEffect(() => {
         const searchActors = async () => {
-            const response = await  tmdbAPI.getListActors(debounce);
+            const response = await tmdbAPI.getListActors(debounce);
             setActorsList(response.results);
         };
         if (debounce) {
@@ -33,7 +35,7 @@ const Home = (props) => {
         setActorName(e.target.value)
     };
 
-    const handleClickOnActorName = (name,id) => {
+    const handleClickOnActorName = (name, id) => {
         setActorName("");
         props.getActorData(actorsList[id]);
         setActorsList([]);
@@ -44,32 +46,64 @@ const Home = (props) => {
         actorName && setActorsListIsShow(true)
     };*/
 
-   return (
-        <>
-            <input type="text" value={actorName} onChange={e => handleChangeActorName(e)} />
-            {
-                actorsList.map((item, id) => (
-                    <div key={item.id} onClick={() => handleClickOnActorName(item.name,id)}>
-                        {item.name}
-                    </div>
-                ))
-            }
-            {
-                props.actors.map((item) => (
-                    <div key={item.id} >
-                        <img src={`https://image.tmdb.org/t/p/w185${item.profile_path}`} alt=""/>
-                        {item.name}
-                    </div>
-                ))
-            }
-           {props.getJointFilms.map((item) => (<div key={item.id} >
+    return (
+        <div>
+            <div>
+                <input type={'text'} value={actorName} onChange={e => handleChangeActorName(e)}/>
+            </div>
+            <div>
+                {
+                    actorsList.map((item, id) => (
+                        <div key={item.id} onClick={() => handleClickOnActorName(item.name, id)}>
+                            {item.name}
+                        </div>
+                    ))
+                }
+                {
+                    props.actors.map((item) => (
+                        <div key={item.id}>
+                            <img src={`https://image.tmdb.org/t/p/w185${item.profile_path}`} alt=""/>
+                            {item.name}
+                        </div>
+                    ))
+                }
+            </div>
+
+
+            {props.getJointFilms.map((item) => (<div key={item.id}>
 
                 {item.original_title}
             </div>))}
 
-        </>
+        </div>
     );
 
 };
+/*
+const Main = styled.main``;
+
+const Wrapper = styled.div`
+    max-width: 1170px;
+    margin: 0 auto;
+    padding: 0 30px;
+    `;
+
+const Search = styled.input`
+    padding: 12px 10px;
+    width: 100%;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+    border: none;
+    outline: none;
+            `;
+
+const ActorsList =styled.div`
+    background-image: url(${background1});
+    background-repeat: repeat-x;
+    background-position: center;
+    background-size: cover;
+`;
+*/
+
 
 export default Home;
