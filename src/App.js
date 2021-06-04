c9cd132bda9edb4d1bf36fbf6418f909e0744d3e
houssston <bobru3@gmail.com>
2021-06-05 02:40:14 +0300

    Добавил footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import HomeContainer from "./pages/Home/HomeContainer";
 import {BrowserRouter} from "react-router-dom";
 import {Route} from "react-router";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <BrowserRouter>
                 <Route path='' render={ () => <HomeContainer/>}/>
             </BrowserRouter>
+            <Footer/>
         </>
     );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './Footer.module.css';
+
+
+const Footer = () => {
+    return (
+        <footer className={s.footer}>
+            <div>All Rights Reserved. Designed By Me</div>
+        </footer>
+    );
+};
+
+export default Footer;
